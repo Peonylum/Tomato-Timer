3cbe0cd4564062d4ae69b3c7080b6de09450259e
Uday Kalra <ukalra@ucsd.edu>
2021-02-09 12:12:07 -0800

    Remove excess semicolons

--- js/example.js
+++ js/example.js
@@ -1,7 +1,5 @@
 // This is an example file to be tested with JEST.
 function sum(a, b) {
-  return a + b;
+  return a + b
 }
-module.exports = sum;
-
-
+module.exports = sum
--- tests/example.test.js
+++ tests/example.test.js
@@ -2,5 +2,5 @@
 const sum = require('../js/example')
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+  expect(sum(1, 2)).toBe(3)
+})
