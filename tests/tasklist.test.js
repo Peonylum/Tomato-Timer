141bc546b3fd33764bb55a7cbbbcfa3467dc1b0f
Dylan Palmer <dcpalmer@ucsd.edu>
2021-03-08 23:03:20 -0800

    tests for updating focused task time

--- tests/tasklist.test.js
+++ tests/tasklist.test.js
@@ -1,47 +1,204 @@
 document.body.innerHTML =
-'<div id="task-container">' +
-'  <div id="task-list">' +
-'    <!-- change color of text during breaks(in CSS file)-->' +
-'    <p>TASK LIST </p>' +
-'    <!-- switch with collapsed arrow on click: src="./assets/collapsedArrow.svg" alt="Collapsed Arrow"-->' +
-'    <img src="./assets/arrrowDropdown.svg" alt="Dropdown">' +
-'  </div>' +
-'  <div id="empty-task-queue">' +
-'    <div id="task-description">' +
-'      <img id="task-icon" src="./assets/taskIcon.svg" alt="task icon">' +
-'      <p>Add a new task and start working.</p>' +
-'    </div>' +
-'    <button id="task-add-button">' +
-'      <img src="./assets/addButton.svg" alt="Plus Button" id="plus-image">' +
-'      Add task' +
-'    </button>' +
-'  </div>' +
-'  <!--Unhide when there are tasks-->' +
-'  <div id="task-queue" style="display: flex;">' +
-'    <!-- <div id="task-queue-header">' +
-'         <p id="pomo-task-header">POMOS</p>' +
-'         <p id="task-header">TASK</p>' +
-'    </div> -->' +
-'    <!-- Append tasks to this task body-->' +
-'    <div id="task-body">' +
-'      <!-- Adding a task-->' +
-'      <div id="adding-task-object">' +
-'        <div id="task-flex">' +
-'          <div id="task-input">' +
-'            <p id="task-header">TASK</p>' +
-'            <input id="pomo-task" name="pomo-task" type="text">' +
-'          </div>' +
+'  <body>' +
+'    <script src="./main.js" defer></script>' +
+'    <section id="nav-bar">' +
+'      <div class="container"> <!--white rectangle behind -->' +
+'        <a id="logo">' +
+'          XVSoft' +
+'          <img src="./assets/logo-image.svg" alt="" id="logo-image">' +
+'        </a>' +
+'        <div id="page-title">' +
+'          <img src="./assets/title-image.svg" alt="" id="title-image">' +
+'          TOMATO' +
 '        </div>' +
-'        <button id="add-task">' +
-'          <img src="assets/addButton.svg" alt="add task" id="add-task-icon">' +
+'        <button id="settings">' +
+'          Settings' +
 '        </button>' +
 '      </div>' +
-'      <!-- Actual Object to be appended-->' +
-'      <div id="tasks">' +
+'    </div>' +
+'  </section>' +
+'  <section id="main">' +
+'    <div id="settings-overlay" style="display: none;">' +
+'      <div id="settings-header">' +
+'        <button id="close-settings">' +
+'          <img id="settings-exit" src="assets/exitButton.svg" alt="exit">' +
+'        </button>' +
+'        <p>Settings</p>' +
+'      </div>' +
+'      <hr>' +
+'      <p>Adjust Time</p>' +
+'      <div id="adjust-time">' +
+'        <div>' +
+'          <label for="pomo-time">Pomo Time:</label>' +
+'          <input class="time-input" id="pomo-time" type="number" step="1" min="1" value="25" max="60">' +
+'        </div>' +
+'        <div>' +
+'          <label for="short-break-time">Short Break Time:</label>' +
+'          <input class="time-input" id="short-break-time" type="number" step="1" min="1" value="25" max="60">' +
+'        </div>' +
+'        <div>' +
+'          <label for="long-break-time">Long Break Time:</label>' +
+'          <input class="time-input" id="long-break-time" type="number" step="1" min="1" value="25" max="60">' +
+'        </div>' +
 '      </div>' +
 '    </div>' +
-'  </div>' +
-'</div>'
+'    <div id="progress-bar">' +
+'      <div id="progress-background">' +
+'        <img id="progress-bar-background" src="./assets/backgroundProgressBar.svg" alt="Progress Bar Background">' +
+'      </div>' +
+'      <div id="filler-bar-1">' +
+'        <svg id="filler-bar-1-svg" width="0" height="8">' +
+'          <rect width="100000" height="6" style="fill:red;" />' +
+'        </svg>' +
+'      </div>' +
+'      <div id="filler-bar-2">' +
+'        <svg id="filler-bar-2-svg" width="0" height="8">' +
+'          <rect width="100000" height="6" style="fill:red;" />' +
+'        </svg>' +
+'      </div>' +
+'      <div>' +
+'        <img id="progress-tomato-1" src="./assets/progressBarTomato.svg" alt="progress bar work tomato">' +
+'      </div>' +
+'      <div>' +
+'        <img id="progress-tomato-2" src="./assets/progressBarTomato.svg" alt="progress bar break tomato">' +
+'      </div>' +
+'      <div>' +
+'        <img id="progress-tomato-3" src="./assets/progressBarTomato.svg" alt="progress bar next! tomato">' +
+'      </div>' +
+'    </div>' +
+'    <div id="timer">' +
+'      <div id="empty-seeds">' +
+'        <img id="seeds" src="./assets/emptySeeds.svg" alt="plain seed">' +
+'      </div>' +
+'      <img id="timer-background" src="./assets/timerBackground.svg" alt="Timer background">' +
+'      <button id="play">' +
+'        <span id="play-button"></span>' +
+'      </button>' +
+'      <button id="stop">' +
+'        <span id="stop-button"></span>' +
+'      </button>' +
+'      <p id="time">25:00</p>' +
+'    </div>' +
+'    <div id="task-container">' +
+'      <div id="task-list">' +
+'	<p>TASK LIST </p>' +
+'	<img src="./assets/arrrowDropdown.svg" alt="Dropdown">' +
+'      </div>' +
+'      <div id="empty-task-queue">' +
+'	<div id="task-description">' +
+'	  <img id="task-icon" src="./assets/taskIcon.svg" alt="task icon">' +
+'	  <p>Add a new task and start working.</p>' +
+'	</div>' +
+'	<button id="task-add-button">' +
+'	  <img src="./assets/addButton.svg" alt="Plus Button" id="plus-image">' +
+'	  Add task' +
+'	</button>' +
+'      </div>' +
+'      <div id="task-queue" style="display: flex;">' +
+'	<div id="task-body">' +
+'	  <!-- Adding a task-->' +
+'	  <div id="adding-task-object">' +
+'	    <div id="task-flex">' +
+'	      <div id="task-input">' +
+'		<p id="task-header">TASK</p>' +
+'		<input id="pomo-task" name="pomo-task" type="text">' +
+'	      </div>' +
+'	    </div>' +
+'	    <button id="add-task">' +
+'	      <img src="assets/addButton.svg" alt="add task" id="add-task-icon">' +
+'	    </button>' +
+'	  </div>' +
+'	  ' +
+'	  <!-- Actual Object to be appended-->' +
+'	  <div id="tasks">' +
+'	  </div>' +
+'	</div>' +
+'      </div>' +
+'    </div>' +
+'    <div id="restart-wrapper">' +
+'      <button id="play-restart">' +
+'	<span id="restart-button"></span>' +
+'      </button>' +
+'    </div>' +
+'    <div class="active" id="onboarding">' +
+'      <div id="onboarding-black"></div>' +
+'      <div id="onboarding-background">' +
+'        <div id="wrapper">' +
+'          <!-- use arrow on leftto hide all this text -->' +
+'          <div>' +
+'            <div id="o1" class="otext">' +
+'              <img src="./assets/image1.png" alt="" id="onboarding-image1">' +
+'              <img src="./assets/image1.png" alt="" id="onboarding-image2">' +
+'              <div class="onboarding-text">' +
+'                <h1>The Pomodoro Technique</h1>' +
+'                <p>The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s.' +
+'                  <br>' +
+'                  <br>' +
+'                  The technique uses a timer to break down work into intervals, traditionally 25 minutes in length,' +
+'                  separated by short breaks.' +
+'                  <br>' +
+'                  <br>' +
+'                  It is named the pomodoro technique after the round tomato timer he used during university, as Pomodoro' +
+'                  means tomato in Italian.' +
+'                </p>' +
+'              </div>' +
+'            </div>' +
+'            <div id="o2" class="otext">' +
+'              <img src="./assets/image1.png" alt="" id="onboarding-image1">' +
+'              <img src="./assets/image1.png" alt="" id="onboarding-image2">' +
+'              <div class="onboarding-text">' +
+'                <h1>How to use our Pomodoro Timer</h1>' +
+'                <p>We made this pomodoro timer to facilitate productivity.' +
+'                  <br>' +
+'                  <br>' +
+'                  ...' +
+'                </p>' +
+'              </div>' +
+'            </div>' +
+'            <div id="o3" class="otext">' +
+'              <img src="./assets/image1.png" alt="" id="onboarding-image1">' +
+'              <img src="./assets/image1.png" alt="" id="onboarding-image2">' +
+'              <div class="onboarding-text">' +
+'                <h1>Step 1</h1>' +
+'                <p>...</p>' +
+'              </div>' +
+'            </div>' +
+'            <div id="o4" class="otext">' +
+'              <img src="./assets/image1.png" alt="" id="onboarding-image1">' +
+'              <img src="./assets/image1.png" alt="" id="onboarding-image2">' +
+'              <div class="onboarding-text">' +
+'                <h1>Step 2</h1>' +
+'                <p>...</p>' +
+'              </div>' +
+'            </div>' +
+'            <div id="o5" class="otext">' +
+'              <img src="./assets/image1.png" alt="" id="onboarding-image1">' +
+'              <img src="./assets/image1.png" alt="" id="onboarding-image2">' +
+'              <div class="onboarding-text">' +
+'                <h1>Step 3</h1>' +
+'                <p>...</p>' +
+'              </div>' +
+'            </div>' +
+'            <div id="o6" class="otext">' +
+'              <img src="./assets/image1.png" alt="" id="onboarding-image1">' +
+'              <img src="./assets/image1.png" alt="" id="onboarding-image2">' +
+'              <div class="onboarding-text">' +
+'                <h1>Start working!</h1>' +
+'                <p>...</p>' +
+'              </div>' +
+'            </div>' +
+'          </div>' +
+'          <div id="onboarding-progress">' +
+'            <img src="./assets/onboarding.svg" alt="" id="onboarding-progress-bar">' +
+'            <button id="onboarding-button">' +
+'              <span id="onboarding-button-img"></span>' +
+'            </button>' +
+'          </div>' +
+'        </div>' +
+'      </div>' +
+'    </div>' +
+'  </section>' +
+'</body>'
 
 const main = require('../source/main')
 
@@ -225,5 +382,39 @@ describe('deleting tasks', () => {
     comp[0].children[0].click()
     expect(list.length).toBe(0)
     expect(comp.length).toBe(0)
+  })
+})
+
+describe('updating focused task time', () => {
+  test('call updateTimer to add to focused task time', () => {
+    main.pomoSession.state = 'work'
+    main.timer.timerLen = 1
+
+    let task_input = document.getElementById('pomo-task')
+    task_input.value = 'test text for our task!'
+    main.addToList()
+    let list = document.getElementsByClassName('task-object')
+    list[0].children[0].click()
+    let focused = document.getElementsByClassName('focused-task')
+    expect(main.focusedTask[0].time).toBe(0)
+
+    main.updateTimer()
+    expect(main.focusedTask[0].time).toBe(1000)
+    focused[0].children[1].click()
+  })
+
+  test('call updateTimer while state is not work', () => {
+    main.timer.timerLen = 0
+    let task_input = document.getElementById('pomo-task')
+    task_input.value = 'test text for our task!'
+    main.addToList()
+    let list = document.getElementsByClassName('task-object')
+    list[0].children[0].click()
+    let focused = document.getElementsByClassName('focused-task')
+    expect(main.focusedTask[0].time).toBe(0)
+
+    main.updateTimer()
+    expect(main.focusedTask[0].time).toBe(0)
+    focused[0].children[1].click()
   })
 })
