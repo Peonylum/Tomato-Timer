5e42ac2d6505e2a4d310363e878399db4d566585
GiantTeddyBearz <giantteddybear1@gmail.com>
2021-03-17 23:58:57 -0700

    attempt to fix linter errors

--- tests/onboarding.test.js
+++ tests/onboarding.test.js
@@ -121,18 +121,18 @@ describe('hideOnClickOutside test', () => {
   })
   it('hideOnClickOutside else', () => {
     const elementMock = { addEventListener: jest.fn(), contains: jest.fn().mockReturnValue(true), setAttribute: jest.fn() }
-    jest.spyOn(document, 'getElementById').mockImplementation(() => elementMock)
-    jest.spyOn(elementMock, 'addEventListener')
-    jest.spyOn(document, 'addEventListener').mockImplementation(() => jest.fn())
-    let mockElement = document.createElement('div')
-    mockElement.setAttribute('id', 'onboarding')
-    mockElement.setAttribute('class', 'active')
-    mockElement = { ...mockElement, contains: jest.fn().mockReturnValue(false) }
-    const mockedFormEvent = { target: { id: 'hello' } }
+    jest.spyOn(document, 'getElementById').mockImplementation(() => elementMock)//
+    jest.spyOn(elementMock, 'addEventListener')//
+    jest.spyOn(document, 'addEventListener').mockImplementation(() => jest.fn())//
+    let mockElement = document.createElement('div')//
+    mockElement.setAttribute('id', 'onboarding')//
+    mockElement.setAttribute('class', 'active')//
+    mockElement = { ...mockElement, contains: jest.fn().mockReturnValue(false) }//
+    const mockedFormEvent = { target: { id: 'hello' } }//
 
-    hideOnClickOutside(document.getElementById('onboarding'), 'play-restart')
-    const handler = document.addEventListener.mock.calls[0][1]
-    handler(mockedFormEvent)
+    hideOnClickOutside(document.getElementById('onboarding'), 'play-restart')//
+    const handler = document.addEventListener.mock.calls[0][1]//
+    handler(mockedFormEvent)//
     expect(elementMock.setAttribute).not.toHaveBeenCalled()
   })
 })
